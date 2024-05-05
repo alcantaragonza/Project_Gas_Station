b5b92d09e461d33f186a96377aff80dd7b602d3c
alcantaragonza <168259148+alcantaragonza@users.noreply.github.com>
2024-05-04 23:56:39 -0600

    Creacion de Clases Principales
    Creacion de las Clases Principales para utilizar durante la realizacion del proyecto

---
+++ Project_Gas_Station/Project_Gas_Station/Clases/Bomba.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Gas_Station.Clases
+{
+    internal class Bomba
+    {
+        private int id;
+        private bool estaOcupada;
+        private double cantidadDispensada;
+
+        public int Id { get => id; set => id = value; }
+        public bool EstaOcupada { get => estaOcupada; set => estaOcupada = value; }
+        public double CantidadDispensada { get => cantidadDispensada; set => cantidadDispensada = value; }
+
+        public void IniciarAbastecimiento(double cantidad)
+        {
+            EstaOcupada = true;
+            CantidadDispensada = cantidad;
+        }
+
+        public void DetenerAbastecimiento()
+        {
+            EstaOcupada = false;
+        }
+    }
+}
